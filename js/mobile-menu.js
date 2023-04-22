79fcb0e9b46a2bb6606ef26f2b2851cb46b83864
SvitlanaParyiska <paryiskasvitlana@gmail.com>
2023-04-22 18:04:34 +0300

    js updated

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,25 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
+// Шукаємо елементи в html розмітці
+const mobileMenu = document.querySelector('[data-menu]');
+const openMenuBtn = document.querySelector('[data-menu-open]');
+const closeMenuBtn = document.querySelector('[data-menu-close]');
+const body = document.querySelector('body');
+// Кінець 1 блоку
 
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+// Функція, яка додає та знімає класи через toggle
+const toggleMenu = () => {
+  mobileMenu.classList.toggle('is-hidden');
+  body.classList.toggle('Sveta');
+};
+// Кінець 2 блоку
 
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-    document.body.classList.toggle('no-scroll');
-  }
-})();
+// Обробник події по кліку мишки
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
+// Кінець 3 блоку
+
+// Close the mobile menu on wider screens if the device orientation changes
+window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  mobileMenu.classList.add('is-hidden');
+  body.classList.remove('Sveta');
+});
